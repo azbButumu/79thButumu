d8aa66545b9a68d391b244b59e562996bf5f1ca8
karurera <eitakurosawa@icloud.com>
2026-04-28 11:49:36 +0900

    product

--- src/pages/tool/product.jsx
+++ src/pages/tool/product.jsx
@@ -1,4 +1,8 @@
+import { useState } from "react"
+import Footer from "../../footer"
 import LikeButton from "./likeButton"
+import './product.css'
+import { SlArrowLeft, SlArrowRight } from "react-icons/sl"
 
 export default function Product({img1,img2,img3,mainTxt,title,like}) {
 
@@ -6,20 +10,41 @@ export default function Product({img1,img2,img3,mainTxt,title,like}) {
         mainTxt = "読み込みエラー"
     }
 
+    const [imgState, setImgState] = useState(3);
+
+    const up = () => {
+        if (imgState < 5) {
+            setImgState(prev => (prev + 1))
+        }
+    }
+
+    const down = () => {
+        if (imgState > 3) {
+            setImgState(prev => (prev - 1))
+        }
+    }
+
   return (
     <>
         <div className = "product">
-            <h2>{title}</h2>
             <div className = "img">
-                <img src = {img1}/>
-                <img src = {img2}/>
-                <img src = {img3}/>
+                <img src = {img1} className = {`state${imgState}`}/>
+                <img src = {img2} className = {`state${imgState - 1}`}/>
+                <img src = {img3} className = {`state${imgState - 2}`}/>
             </div>
+            <div className = "state">
+                <div className = "stateup" onClick={down}/>
+                <div className = "statedown" onClick={up}/>
+            </div>
+            <h2>{title}</h2>
             <div className = "mainTxt">
-                {mainTxt}
+                <p>{mainTxt}</p>
             </div>
-            <LikeButton pageId = {like}/>
+            <div className = "proLike">
+                <LikeButton pageId = {like}/>
+            </div>
         </div>  
+        <Footer/>
     </>
   )
 }
